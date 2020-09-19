32ceaf85e0d7bd27898d6ac8bbf7c7f99459f86b
zkneupper <zachary.kneupper@gmail.com>
2020-09-19 13:15:16 -0400

    Import from codecolor into __init__

--- codecolor/__init__.py
+++ codecolor/__init__.py
@@ -3,6 +3,14 @@
 
 """Top-level package for codecolor."""
 
-__author__ = """Zachary Jonathan Kneupper"""
-__email__ = '""'
+__author__ = "Zachary Jonathan Kneupper"
+__email__ = ''
 __version__ = '0.0.1'
+
+
+from .codecolor import (
+	get_all_styles,
+	highlight_code,
+	getsource,
+	printsource,
+)
