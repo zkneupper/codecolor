5f08adc391e83a037844790f5a65645b45300a26
zkneupper <zachary.kneupper@gmail.com>
2020-09-20 16:58:34 -0400

    Add module docstring to codecolor/__init__.py

--- codecolor/__init__.py
+++ codecolor/__init__.py
@@ -1,7 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-"""Top-level package for codecolor."""
+
+"""codecolor
+~~~~~~~~
+
+Codecolor adds color when inspecting source code of python objects.
+It applies syntax highlighting to object source code.
+
+The primary function of the codecolor module is the `printsource`
+function. The `printsource(obj, style="default")` function prints
+the syntax-highlighted source code for the python object `obj`.
+
+Codecolor combines the code inspection functionality of
+`inspect.getsource()` and syntax highlighting functionality
+of the `pygments` package.
+
+
+Example:
+    How does the pytorch relu function work under the hood?
+    You can easily inspect the source code of the pytorch relu function
+    by using `codecolor.printsource(relu)` to to print the source code of
+    the relu function with syntax highlighting.
+
+    >>> from codecolor import printsource
+    >>> from torch.nn.functional import relu
+    >>> printsource(relu)
+    # prints the source code of the relu function with syntax highlighting
+
+Functions:
+
+    printsource(obj, style="default")
+
+    getsource(obj, style="default")
+
+    highlight_code(code, style="default")
+
+    get_all_styles()
+
+"""
+
 
 __author__ = "Zachary Jonathan Kneupper"
 __email__ = ""
